cd7586ae90c6a84200861020e6318db06bac9a37
JJH <jeww1234@gmail.com>
2025-10-25 22:49:56 +0900

    city

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import { call_api } from "./utils/call_api";
 import Weather from "./component/Weather/Weather";
 import WeatherBtn from "./component/WeatherBtn/WeatherBtn";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const API_KEY = "0b455b67f7ff2cd45dcccd33831a3728";
 
@@ -15,31 +14,46 @@ function App() {
   //커런트 버튼을 클릭하면 현재 위치 기반의 날시가 나온다
   //데이터를 들고오는 동안 로딩 스피너
 
-  const [weatherData, setWeatherData] = useState(null)
+  const [weatherData, setWeatherData] = useState(null);
+  const cities = ["Paris", "New York", "Tokyo", "Seoul"];
+  const [city, setCity] = useState("");
 
   const getCurrentLocation = () => {
     navigator.geolocation.getCurrentPosition(async (position) => {
       let lat = position.coords.latitude;
       let lon = position.coords.longitude;
       console.log(lat, lon);
-      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+      let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
       console.log(url);
-      const result = await call_api(url);
-      console.log(result)
-      setWeatherData(result)
+      let result = await call_api(url);
+      console.log(result);
+      setWeatherData(result);
     });
   };
 
-  useEffect(() => {
-    getCurrentLocation();
-  }, []);
+  const getWeatherByCity = useCallback(async () => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    let result = await call_api(url);
+    console.log("city data", result);
+    setWeatherData(result);
+  }, [city]);
 
-  return <div>
-    <div className="container">
-    <Weather weatherData={weatherData}/>
-    <WeatherBtn />
+  useEffect(() => {
+    if (city === "") {
+      getCurrentLocation();
+    } else {
+      getWeatherByCity();
+    }
+  }, [city, getWeatherByCity]);
+
+  return (
+    <div>
+      <div className="container">
+        <Weather weatherData={weatherData} />
+        <WeatherBtn setCity={setCity} cities={cities} getCurrentLocation={getCurrentLocation}/>
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default App;
--- src/component/WeatherBtn/WeatherBtn.jsx
+++ src/component/WeatherBtn/WeatherBtn.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 import "./WeatherBtn.style.css";
 
-const WeatherBtn = () => {
+const WeatherBtn = ({ setCity, cities, getCurrentLocation }) => {
   return (
     <div className="weather-btn">
-      <Button variant="warning">Current</Button>
-      <Button variant="warning">Paris</Button>
-      <Button variant="warning">New York</Button>
-      <Button variant="warning">Tokyo</Button>
+      <Button variant="warning" onClick={()=>getCurrentLocation()}>
+          Current
+        </Button>
+      {cities.map((item, index) => (
+        <Button variant="warning" key={index} onClick={() => setCity(item)}>
+          {item}
+        </Button>
+      ))}
     </div>
   );
 };
